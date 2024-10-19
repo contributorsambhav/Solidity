1b0b20ecadba3995c950c41953fef196a78a6071
contributorsambhav <Sambhav511974@gmail.com>
2024-10-20 00:54:59 +0530

    datatypes

---
+++ datatypes.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract datatypes {
+    // bool public yesno = true; // default false
+
+    //uint is unsigned integer
+
+    // uint8 public u8 = 255; // 2^8 -1; less gas
+    // uint256 public u256 = 85; // by default uint256 more gas
+
+    // negative numbers are allowed in int type
+    // int8 public  ate; // defaultfalse
+    // int8 public signed = -52; // -2^(8-1) -1 to +2^(8-1)
+    // int public minInt = type(int).min;
+    // int public maxInt = type(int).max;
+
+    //ARRAY reference data types
+    // fixed sized byte array
+    //dynamic sized byte array (default)
+
+    // bytes1 public a = 0xb5;
+    // bytes1 public b;// default 0x00
+
+
+    // address
+
+    // address public  hey ; 
+    // address public  old = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    
+}
